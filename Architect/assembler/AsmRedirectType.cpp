3409753a037437fd7d9bd6da563309819d89b7c6
void <juozuxp@gmail.com>
2023-08-15 23:33:11 +0200

    Overviewed moffs definition

--- Architect/assembler/AsmRedirectType.cpp
+++ Architect/assembler/AsmRedirectType.cpp
@@ -11,7 +11,7 @@ std::shared_ptr<const AsmIndex> AsmRedirectType::GetEntry(const ILInstruction& i
 
 	uint8_t index = static_cast<uint8_t>(c_ILTypeToType[operand.m_Type]);
 	if (operand.m_Type == ILOperandType_MemoryAbsolute &&
-		operand.m_Scale != ILOperandScale_64)
+		static_cast<uint64_t>(operand.m_MemoryValue.m_Value) < static_cast<uint64_t>(UINT32_MAX))
 	{
 		index = static_cast<uint8_t>(IndexOpType::mem);
 	}
